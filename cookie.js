53fdcac527def487e0da387698e3dd0d4a334a83
Asanda Excellent Ngcobo <105105413+Asanda-Ngcobo@users.noreply.github.com>
2023-06-04 03:52:40 -0700

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -31,4 +31,27 @@ getout.addEventListener('click', () => {
 
 
 });
+
+// Check if the visitor's country matches the allowed country
+function checkCountry() {
+  // Replace 'XX' with the country code you want to allow
+  const allowedCountry = 'ZA';
+  
+  // Make an API call to get the visitor's country
+  fetch('https://api.ipgeolocationapi.com/geolocate')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      const visitorCountry = data.country_code2;
+      
+      // Redirect if the visitor's country doesn't match the allowed country
+      if (visitorCountry !== allowedCountry) {
+        window.location.href = 'https://ecolie.co.za/restricted.html'; // Replace with your restricted page URL
+      }
+    })
+    .catch(function(error) {
+      console.error('Error:', error);
+    });
+}
    
